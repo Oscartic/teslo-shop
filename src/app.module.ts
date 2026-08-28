caca8b6316636f7256f444fca1282dd10cb3acff
OscarB <oscartic@gmail.com>
2026-08-28 16:02:25 -0400

    Initialize project with configuration and setup files
    Set up foundational configuration for a new NestJS project with ESLint and Prettier. Added Docker support with PostgreSQL and environment variable templates for seamless development setup.

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST_DB,
+      port: +process.env.PORT_DB!,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
+})
+export class AppModule {}

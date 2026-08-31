8f34d9521017d1b3958c072ea85c871509c9c4c3
OscarB <oscartic@gmail.com>
2026-08-31 18:28:20 -0400

    chore: Define Product entity for database interaction

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,53 @@
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column('text', {
+        unique: true
+    })
+    title!: string;
+
+    @Column('float', {
+        default: 0
+    })
+    price!: number;
+
+    @Column('text', {
+        nullable: true,
+    })
+    description!: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug!: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock!: number;
+
+    @Column('text', {
+        array: true,
+    })
+    sizes!: string[];
+
+    @Column('text')
+    gender!: string;
+
+    //tags
+    //images
+
+    @BeforeInsert()
+    checkSlugInsert() {
+        if(!this.slug) {
+            this.slug = this.title.toLowerCase().replaceAll(' ', '_');
+        } else {
+            this.slug = this.slug.toLowerCase().replaceAll(' ', '_');
+        }
+    }
+    // @BeforeUpdate
+}

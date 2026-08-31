aa1c4d51a76ad46cfffe5c54df91574e3e49d48e
OscarB <oscartic@gmail.com>
2026-08-31 18:28:20 -0400

    feat: Implement ProductsService with core product functionalities

---
+++ src/products/products.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, InternalServerErrorException, BadRequestException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger('«ProductsService»');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) {
+  
+  }
+
+  async create(createProductDto: CreateProductDto) {
+   try {
+    const product = this.productRepository.create(createProductDto);
+    await this.productRepository.save(product);
+    return product;
+   } catch (error) {
+     this.handleDBExceptions(error);
+   }
+
+  }
+
+  findAll() {
+    try {
+      return this.productRepository.find();
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+
+    if(!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+  }
+
+  private handleDBExceptions(error: any) {
+    if(error.code === '23505') {
+      throw new BadRequestException(`Error creating product. ${error.detail}`);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error.');
+  }
+}

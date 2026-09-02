ba98b2e9f25727edf20e9f2d4110fdf67293ab54
OscarB <oscartic@gmail.com>
2026-09-01 20:06:54 -0400

    feat: Update ProductsController to use pagination and find by search

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from '../common/dtos/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -13,17 +14,21 @@ export class ProductsController {
   }
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  findAll(@Query() paginationDto: PaginationDto) {
+    // console.log('Pagination DTO:', paginationDto);
+    return this.productsService.findAll(paginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.productsService.findOne(id);
+  @Get(':search')
+  findOne(@Param('search') search: string) {
+    return this.productsService.findOne(search);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto
+  ) {
     return this.productsService.update(id, updateProductDto);
   }
 

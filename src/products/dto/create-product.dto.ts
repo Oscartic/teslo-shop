2c7cf57e875efda65fe71825265d77a4bca60e8c
OscarB <oscartic@gmail.com>
2026-09-02 11:18:32 -0400

    chore: Add tags property to CreateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -39,4 +39,9 @@ export class CreateProductDto {
 
     @IsIn(['men', 'women', 'kid', 'unisex'])
     gender!: string;
+
+    @IsArray()
+    @IsString({ each: true })
+    @IsOptional()
+    tags?: string[];
 }

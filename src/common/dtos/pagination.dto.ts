4ea9a66aa39765739837b9e05719856cf5435be1
OscarB <oscartic@gmail.com>
2026-09-11 16:23:37 -0300

    docs: Annotate DTOs with Swagger decorators for better schema generation

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,17 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto {
+    @ApiProperty({
+        default: 10,
+        description: 'The page number for pagination'
+    })
     @IsOptional()
     @IsPositive()
     @Type(() => Number) // enable implicit conversion of query params to number
     page?: number;
     
+    @ApiProperty({
+        example: 10,
+        description: 'The number of items per page for pagination'
+    })
     @IsOptional()
     @IsPositive()
     @Type(() => Number)
     limit?: number;
     
+    @ApiProperty({
+        example: 0,
+        description: 'The offset for pagination'
+    })
     @IsOptional()
     @Min(0)
     @Type(() => Number)

b891076b71e997f1d7b98b9cd3eb3dd5066a5f6c
OscarB <oscartic@gmail.com>
2026-09-11 13:17:01 -0300

    chore: Establish one-to-many relation between User and Product entities

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate, OneToMany } from 'typeorm';
+import { Product } from '../../products/entities';
 
 @Entity('users')
 export class User {
@@ -28,6 +29,12 @@ export class User {
         default: ['user'],
     })
     role!: string[];
+
+    @OneToMany(
+        () => Product,
+        (product) => product.user,
+    )
+    product!: Product[];
 
     @BeforeInsert()
     checkFieldsBeforeInsert() {
